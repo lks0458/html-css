46ee2e4dda2a753e6d6d81d707bef4710cdf698f
lks <tndrhkd12@naver.com>
2021-08-08 20:21:50 +0900

    first html css

---
+++ main.js
@@ -0,0 +1,25 @@
+// let Producer = document.querySelector("#Producer");
+// let Work = document.querySelector("#Work");
+
+// Producer.onclick = function(){
+//     window.scrollTo(0,1397);
+// }
+
+// let headtitle = document.querySelector(".sub");
+
+// let moveProducer = document.querySelector("#Producer");
+
+// moveWork.addEventListener("click", ()=>{
+
+// })
+
+// document.addEventListener('scroll', function() {
+//     document.documentElement.scrollTop;
+// });
+
+// 800, 1397
+
+// $("#Producer").click(function(){
+//     $("html, body").animate({scrollTop: 1397}, "slow");
+//     return false;
+// });
